3ee96e11c42b332736362ff4784d11dd6248be12
Akshaya Reddy <24wh1a05b0@bvrithyderabad.edu.in>
2026-06-20 09:16:21 +0530

    toggle and card

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,3 +1,25 @@
 //alert("Hello From JS !");
 
 console.log("Hello From JS !");
+const btn = document.getElementById("toggleBtn");
+const image = document.getElementById("userImage");
+const name = document.getElementById("userName");
+const gender = document.getElementById("userGender");
+
+let isMale = true;
+
+btn.addEventListener("click", () => {
+    if (isMale) {
+        image.src = "../images/jane.png"; // female image
+        name.textContent = "Jane Doe";
+        gender.textContent = "Female";
+        btn.textContent = "Show Male";
+    } else {
+        image.src = "../images/john.png";
+        name.textContent = "John Doe";
+        gender.textContent = "Male";
+        btn.textContent = "Show Female";
+    }
+
+    isMale = !isMale;
+});
