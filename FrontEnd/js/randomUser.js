90ff1aa26a17279dc70ef1baacb68af30675f158
Akshaya Reddy <24wh1a05b0@bvrithyderabad.edu.in>
2026-06-20 10:30:20 +0530

    Random Users

---
+++ FrontEnd/js/randomUser.js
@@ -0,0 +1,19 @@
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            var user = data.results[0];
+            var fullName = user.name.title + " " + user.name.first + " " + user.name.last;
+            document.getElementById("user-Image").src = user.picture.large;
+
+            document.getElementById("user-Name").innerText = fullName;
+
+            document.getElementById("user-Gender").innerText = user.gender;
+        })
+
+        .catch(function(err){
+            console.log("Error : " + err);
+        })
+}
